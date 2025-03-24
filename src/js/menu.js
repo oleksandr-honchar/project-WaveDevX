f082bd5ad3bcd83665e4c1ab8520f14462245ac3
Artem <tem4ik20.1@gmail.com>
2025-03-24 21:27:07 +0200

    fix-bug-about-us and header-nav-btn

--- src/js/menu.js
+++
@@ -1,9 +0,0 @@
-// Відкриття меню
-document.querySelector(".menu-btn").addEventListener("click", function() {
-    document.querySelector(".mobile-menu").classList.add("is-open");
-});
-
-// Закриття меню
-document.querySelector(".close-btn").addEventListener("click", function() {
-    document.querySelector(".mobile-menu").classList.remove("is-open");
-});
