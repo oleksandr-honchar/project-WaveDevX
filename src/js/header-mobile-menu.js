0d3cea6fc8b02d37d0a80e4b2008390c16cd172a
Artem <tem4ik20.1@gmail.com>
2025-03-24 21:40:26 +0200

    deleted-unnecessary-comment

--- src/js/header-mobile-menu.js
+++ src/js/header-mobile-menu.js
@@ -1,35 +1,24 @@
 const menu = document.querySelector(".mobile-menu");
 const openBtn = document.querySelector(".menu-btn");
 const closeBtn = document.querySelector(".close-btn");
-const menuLinks = document.querySelectorAll(".menu-list-link"); // Отримуємо всі посилання в меню
+const menuLinks = document.querySelectorAll(".menu-list-link"); 
 
-// Відкриття меню
 openBtn.addEventListener("click", function () {
     menu.classList.add("is-open");
-    menu.style.opacity = "1"; 
-    menu.style.transform = "translateX(0)"; 
-    menu.style.pointerEvents = "auto"; 
 });
 
-// Закриття меню при натисканні на кнопку "Закрити"
 closeBtn.addEventListener("click", function () {
     closeMenu();
 });
 
-// Закриття меню при натисканні на будь-який пункт меню
 menuLinks.forEach(link => {
     link.addEventListener("click", function () {
         closeMenu();
     });
 });
 
-// Функція для закриття меню з анімацією
 function closeMenu() {
-    menu.style.opacity = "0"; // Починає зменшувати прозорість
-    menu.style.transform = "translateX(100%)"; // Переміщає меню вправо
-
     setTimeout(() => {
-        menu.classList.remove("is-open"); // Видаляє клас після завершення анімації
-        menu.style.pointerEvents = "none"; // Забороняє взаємодію після закриття
-    }, 400); // Час затримки має збігатися з transition у CSS
+        menu.classList.remove("is-open"); 
+    }, 400); 
 }
